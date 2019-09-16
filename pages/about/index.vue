f29b78df29212015dbbff79d7692ea2af402e365
laiht <haitao.lai@wehotelglobal.com>
2019-09-16 16:22:13 +0800

    初始化项目

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+	about page
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+	home page
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
